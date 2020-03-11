ea2f82ac0fe060b4a80fa2c5931e366620711da9
vagrant <vagrant@homestead>
2020-03-11 06:13:54 +0000

    用户可以上传话题图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -71,7 +71,24 @@
     $(document).ready(function() {
       var editor = new Simditor({
         textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params: {
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount: 3,
+          leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+        },
+        pasteImage: true,
       });
     });
   </script>
 @stop
+<!--
+pasteImage —— 设定是否支持图片黏贴上传，这里我们使用 true 进行开启；
+url —— 处理上传图片的 URL；
+params —— 表单提交的参数，Laravel 的 POST 请求必须带防止 CSRF 跨站请求伪造的 _token 参数；
+fileKey —— 是服务器端获取图片的键值，我们设置为 upload_file;
+connectionCount —— 最多只能同时上传 3 张图片；
+leaveConfirm —— 上传过程中，用户关闭页面时的提醒。 -->
