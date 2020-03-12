f4d6243da7f2466a6ccbdc0a740219bc55b62bc9
vagrant <vagrant@homestead>
2020-03-12 01:39:31 +0000

    用户可以发表评论

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -64,36 +64,14 @@
         </div>
       </div>
 
-      {{-- 用户回复列表 --}}
+       {{-- 用户回复列表 --}}
       <div class="card topic-reply mt-4">
           <div class="card-body">
-              @include('topics._reply_box', ['topic' => $topic])
+              @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
               @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
           </div>
       </div>
-    {{-- 用户发布的内容 --}}
-    <div class="card ">
-      <div class="card-body">
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
-            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('users.show', $user->id) }}">
-              Ta 的话题
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
-              Ta 的回复
-            </a>
-          </li>
-        </ul>
-        @if (if_query('tab', 'replies'))
-          @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
-        @else
-          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
-        @endif
-      </div>
-    </div>
 
     </div>
-</div>
+  </div>
 @stop
